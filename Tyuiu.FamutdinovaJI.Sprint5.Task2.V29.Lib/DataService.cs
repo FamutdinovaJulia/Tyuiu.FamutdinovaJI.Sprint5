09b47475e334f62f42a63c0151ee6164ef6cf7cb
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-24 05:01:07 +0500

    Task2

---
+++ Tyuiu.FamutdinovaJI.Sprint5.Task2.V29.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FamutdinovaJI.Sprint5.Task2.V29.Lib
+{
+    public class DataService : ISprint5Task2V29
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.GetTempFileName();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string res = "";
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == matrix.GetLength(1) - 1)
+                    {
+                        res = res + matrix[i, j];
+                    }
+                    else
+                    {
+                        res = res + matrix[i, j] + ';';
+                    }
+
+                }
+                File.AppendAllText(path, res + Environment.NewLine);
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FamutdinovaJI.Sprint5.Task2.V29/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint5.Task2.V29.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint5.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = { { 9, 2, 5 }, { 3, 2, 4 }, { 2, 8, 8 } };
+
+            Console.WriteLine("*  = " + matrix);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(matrix);
+            StreamReader sr = new StreamReader(res);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine();
+
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+        }
+    }
+}
