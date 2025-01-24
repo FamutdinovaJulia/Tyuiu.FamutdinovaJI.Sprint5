1aaeb61494e4f3a1e07dbf76a6bbd77006b90dca
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-24 05:45:51 +0500

    0

---
+++ Tyuiu.FamutdinovaJI.Sprint5.Task4.V29.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FamutdinovaJI.Sprint5.Task4.V29.Lib
+{
+    public class DataService : ISprint5Task4V29
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+            var fmt = new NumberFormatInfo();
+            fmt.NegativeSign = "−";
+            var x = double.Parse(strx, fmt);
+            double res = (x / (2 * x)) + Math.Sin(x * x);
+            res = Math.Round(res, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.FamutdinovaJI.Sprint5.Task4.V29/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint5.Task4.V29.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint5.Task4.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetTempFileName();
+            string y = "4";
+            File.AppendAllText(path, y);
+            DataService ds = new DataService();
+            Console.WriteLine(ds.LoadFromDataFile(path));
+        }
+    }
+}
